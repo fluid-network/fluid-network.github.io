57c94b8b56d86e608ba3355a3a651d49f3fddcfc
fluid-network <95925506+fluid-network@users.noreply.github.com>
2023-07-10 16:56:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+var mouse = {
+    x: undefined,
+    y: undefined,
+    scroll: {
+        x: undefined,
+        y: undefined,
+        deltaX: 0,
+        deltaY: 0
+    }
+};
+var topsection = {
+    element: document.getElementById('top-section'),
+    translateY: 0
+};
+
+// Scroll bar animation
 {
-var div = document.getElementById('top-section-section2'), topsectionanimation = {animationId: undefined, dir: 1, status: 0};
+var div = document.getElementById('top-section-section2'), topsectionanimation = {animationId: undefined, dir: 0.5, status: 0};
 function startScrollAnimation() {
     topsectionanimation.animationId = requestAnimationFrame(scrollContent);
     topsectionanimation.status = 1;
@@ -11,16 +27,16 @@ function stopScrollAnimation() {
 function scrollContent() {
     if(div.scrollLeft === (div.scrollWidth - div.clientWidth)) {
         setTimeout(function(event) {
-            topsectionanimation.dir = -1;
+            topsectionanimation.dir = -0.75;
         }, 1000);
         
     } else if(div.scrollLeft === 0) {
         setTimeout(function(event) {
-            topsectionanimation.dir = 1;
+            topsectionanimation.dir = 0.75;
         }, 1000);
     }
     
-    if (!(div.scrollLeft === 0 && topsectionanimation.dir === -1) && !(div.scrollLeft === div.scrollWidth - div.clientWidth && topsectionanimation.dir === 1)) {
+    if (!(div.scrollLeft === 0 && topsectionanimation.dir === -0.75) && !(div.scrollLeft === div.scrollWidth - div.clientWidth && topsectionanimation.dir === 0.75)) {
         div.scrollLeft += topsectionanimation.dir;
     } 
     
@@ -43,15 +59,33 @@ div.addEventListener('touchstart', function(event) {
 });
 document.addEventListener('mouseup', function(event) {
     if(topsectionanimation.status === 0) {
-        startScrollAnimation();
+        setTimeout(() => {
+            startScrollAnimation();
+        }, 1000);
+        
     }
 });
 div.addEventListener('touchend', function(event) {
     if(!div.matches(':active') && topsectionanimation.status === 0) {
-        startScrollAnimation();
+        setTimeout(() => {
+            startScrollAnimation();
+        }, 1000);
     }
 });
 }
+
+// top-section movement
+document.addEventListener('scroll', (event) => {
+    mouse.scroll.deltaX = window.scrollX - (mouse.scroll.x || window.scrollX);
+    mouse.scroll.deltaY = window.scrollY - (mouse.scroll.y || window.scrollY);
+    mouse.scroll.x = window.scrollX;
+    mouse.scroll.y = window.scrollY;
+
+    topsection.translateY -= mouse.scroll.deltaY;
+    topsection.translateY = Math.min(Math.max(topsection.translateY, -topsection.element.clientHeight), 0);
+    document.getElementById('top-section').style.transform = `translateY(${topsection.translateY}px)`;
+});
+
 window.onload = function(event) {
     startScrollAnimation();
 };
