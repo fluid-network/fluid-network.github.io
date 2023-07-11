312019bd26bb47486566b4f45cf199c92b6802f6
fluid-network <95925506+fluid-network@users.noreply.github.com>
2023-07-11 13:48:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var mouse = {
-    x: undefined,
-    y: undefined,
+    position: {
+        x: undefined,
+        y: undefined,
+        deltaX: 0,
+        deltaY: 0
+    },
     scroll: {
         x: undefined,
         y: undefined,
@@ -10,7 +14,13 @@ var mouse = {
 };
 var topsection = {
     element: document.getElementById('top-section'),
-    translateY: 0
+    translateY: 0,
+    menubutton: document.getElementById('top-section-menu-button'),
+    menu: {
+        element: document.getElementById('top-section-menu'),
+        slidebar: document.getElementById('top-section-menu-slide-bar'),
+        exitbutton: document.getElementById('top-section-menu-exit-button')
+    }
 };
 
 // Scroll bar animation
@@ -85,6 +95,59 @@ document.addEventListener('scroll', (event) => {
     topsection.translateY = Math.min(Math.max(topsection.translateY, -topsection.element.clientHeight), 0);
     document.getElementById('top-section').style.transform = `translateY(${topsection.translateY}px)`;
 });
+
+// top-section-menu movement
+topsection.menu.slidebar.addEventListener('touchmove', (event) => {
+    if(topsection.menu.element.style.transition !== '0s') {
+        topsection.menu.element.style.transition = '0s';
+    }
+    mouse.position.deltaX = event.touches[event.touches.length - 1].clientX - (mouse.position.x || event.touches[event.touches.length - 1].clientX);
+    mouse.position.deltaY = event.touches[event.touches.length - 1].clientY - (mouse.position.y || event.touches[event.touches.length - 1].clientY);
+    mouse.position.x = event.touches[event.touches.length - 1].clientX;
+    mouse.position.y = event.touches[event.touches.length - 1].clientY;
+    topsection.menu.slidebar.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    topsection.menu.element.style.transform = `translateX(${(Math.min(Math.max(mouse.position.x, 0), topsection.menu.element.clientWidth) - topsection.menu.element.clientWidth)}px)`;
+});
+topsection.menu.slidebar.addEventListener('touchend', (event) => {
+    topsection.menu.element.style.transition = '0.3s';
+    topsection.menu.slidebar.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    if(Math.min(Math.max(mouse.position.x, 0), topsection.menu.element.clientWidth) < topsection.menu.element.clientWidth * 0.7 && mouse.position.deltaX <= 0) {
+        topsection.menu.element.style.transform = 'translateX(-100%)';
+    } else if(mouse.position.deltaX < -5) {
+        topsection.menu.element.style.transform = 'translateX(-100%)';
+    } else {
+        topsection.menu.element.style.transform = 'translateX(0%)';
+    }
+});
+document.addEventListener('mousemove', (event) => {
+    if(topsection.menu.slidebar.matches(':active') === true) {
+        if(topsection.menu.element.style.transition !== '0s') {
+            topsection.menu.element.style.transition = '0s';
+        }
+        mouse.position.deltaX = event.clientX - (mouse.position.x || event.clientX);
+        mouse.position.deltaY = event.clientY - (mouse.position.y || event.clientX);
+        mouse.position.x = event.clientX;
+        mouse.position.y = event.clientY;
+        topsection.menu.element.style.transform = `translateX(${(Math.min(Math.max(mouse.position.x, 0), topsection.menu.element.clientWidth) - topsection.menu.element.clientWidth)}px)`;
+    }
+});
+document.addEventListener('mouseup', (event) => {
+    if(topsection.menu.element.style.transition !== '0.3s') {
+        topsection.menu.element.style.transition = '0.3s';
+    }
+});
+topsection.menubutton.addEventListener('click', (event) => {
+    if((Math.min(Math.max(mouse.position.x, 0), topsection.menu.element.clientWidth) - topsection.menu.element.clientWidth) === -topsection.menu.element.clientWidth || topsection.menu.element.style.transform === 'translateX(-100%)') {
+        topsection.menu.element.style.transform = 'translateX(0%)';
+    } else {
+        topsection.menu.element.style.transform = 'translateX(-100%)';
+    }
+});
+topsection.menu.exitbutton.addEventListener('click', (event) => {
+    topsection.menu.element.style.transform = 'translateX(-100%)';
+});
+
+
 
 window.onload = function(event) {
     startScrollAnimation();
